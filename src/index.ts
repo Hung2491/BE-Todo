b794dbb95e001000783ae4fde023b2e085a8e133
Nguyen Tung <tung.nd@x-or.cloud>
2025-10-09 14:00:34 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import taskRoutes from './routes/taskRoutes';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/ts-crud')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/tasks', taskRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
